03f30d4bbab72a373e8383057c84127cfe998904
jianying.wcj <jianying.wcj@taobao.com>
2015-01-29 22:57:54 +0800

    基础测试用例

--- src/main/java/TimeClient.java
+++ src/main/java/TimeClient.java
@@ -1,11 +1,13 @@
 import io.netty.bootstrap.Bootstrap;
-import io.netty.channel.EventLoop;
-import io.netty.channel.EventLoopGroup;
+import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.util.concurrent.EventExecutor;
 import io.netty.util.concurrent.Future;
 import io.netty.util.concurrent.ScheduledFuture;
 
+import java.text.NumberFormat;
 import java.util.Collection;
 import java.util.List;
 import java.util.Set;
@@ -22,7 +24,30 @@ public class TimeClient {
         EventLoopGroup group = new NioEventLoopGroup();
         try {
             Bootstrap b = new Bootstrap();
-           // b.group(group).channel()
+            b.group(group).channel(NioSocketChannel.class)
+                    .option(ChannelOption.TCP_NODELAY,true)
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        public void initChannel(SocketChannel ch) {
+                               ch.pipeline().addLast(new TimeClientHandler());
+                        }
+                    });
+            ChannelFuture f = b.connect(host,port).sync();
+            f.channel().closeFuture().sync();
+        } finally {
+            group.shutdownGracefully();
         }
+    }
+
+    public static void main(String[] args) throws Exception {
+        int port = 8080;
+        if(args != null && args.length > 0) {
+            try {
+                port = Integer.valueOf(args[0]);
+            } catch(NumberFormatException e) {
+                //
+            }
+        }
+        new TimeClient().connect(port,"127.0.0.1");
     }
 }
